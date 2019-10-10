33eb4cefc8b37d1451ace151222a770411d1a001
mb706 <mlr.developer@mb706.com>
2019-10-10 14:34:10 +0200

    bugfixes

--- R/autoxgboost.R
+++ R/autoxgboost.R
@@ -6,8 +6,6 @@
 #'
 #' @param task\cr
 #'   The task.
-#' @param par.set\cr
-#'   Which paramset to use. either \dQuote{unmixed} (default) or \dQuote{mixed} (larger parameter set) but with param dependencies.
 #' @param predict.type\cr
 #'   Predict type. One of \dQuote{prob}, \dQuote{response} (default). Only \dQuote{response} supported for regression tasks.
 #' @param max.nrounds\cr
@@ -16,7 +14,7 @@
 #'   After how many iterations without an improvement in the boosting OOB error should be stopped?
 #'   Default is \code{10}.
 #' @param early.stopping.fraction\cr
-#'   What fraction of the data should be used for early stopping (i.e. as a validation set).
+#'   What fraction of the data should be used for early stopping (i.e. as a training set). This is apparently just the holdout split for optimization.
 #'   Default is \code{4/5}.
 #' @param impact.encoding.boundary\cr
 #'   Defines the threshold on how factor variables are handled. Factors with more levels than the \code{"impact.encoding.boundary"} get impact encoded while factor variables with less or equal levels than the \code{"impact.encoding.boundary"} get dummy encoded.
@@ -33,13 +31,12 @@
 #'   while a more exact approach would be to apply it only to the training set (emulate_exactly==FALSE).
 #' @return \code{something}.
 #' @export
-autoxgboost_space = function(task, par.set = "unmixed", predict.type = "response",
+autoxgboost_space = function(task, predict.type = "response",
   max.nrounds = 1e6, early.stopping.rounds = 10, early.stopping.fraction = 4/5,
   impact.encoding.boundary = 10, nthread = NULL, tune.threshold = TRUE, emulate_exactly = TRUE) {
 
   # check inputs
   assertClass(task, "TaskSupervised")
-  assertChoice(par.set, c("unmixed", "mixed"))
   assertChoice(predict.type, c("response", "prob"))
   assertIntegerish(max.nrounds, lower = 1L, len = 1L)
   assertIntegerish(early.stopping.rounds, lower = 1L, len = 1L)
@@ -67,29 +64,27 @@ autoxgboost_space = function(task, par.set = "unmixed", predict.type = "response
 
   lapply(list(
     ParamFct$new("xgboost.booster", c("gbtree", "gblinear", "dart")),
-    ParamFct$new("xgboost.sampler_type", c("uniform", "weighted")),
+    ParamFct$new("xgboost.sample_type", c("uniform", "weighted")),
     ParamFct$new("xgboost.normalize_type", c("tree", "forest")),
     ParamDbl$new("xgboost.rate_drop", 0, 1),
-    ParamDbl$new("xgboost.skip_drop", 0, 1),
-    ParamLgl$new("xgboost.one_drop"),
-    ParamFct$new("xgboost.grow_policy", c("depthwise", "lossguide")),
-    ParamInt$new("xgboost.max_leaves", 0, 8),
-    ParamInt$new("xgboost.max_bin", 2, 9)
+    ParamDbl$new("xgboost.skip_drop", 0, 1)
+    # the following depends a bit on https://github.com/mlr-org/mlr3learners/issues/45
+    # although for random search they should not make a difference.
+#    ParamLgl$new("xgboost.one_drop"),  # one_drop has "almost surely" no effect; it does not exist in mlr3learners yet.
+#    ParamFct$new("xgboost.grow_policy", c("depthwise", "lossguide")),  # has no effect apparently because tree_method is not 'hist'
+#    ParamInt$new("xgboost.max_leaves", 0, 8),  # has no effect apparently because tree_method is not 'hist'
+#    ParamInt$new("xgboost.max_bin", 2, 9)  # has no effect apparently because tree_method is not 'hist'
   ), autoxgbparset.mixed$add)
 
   autoxgbparset.mixed$
-    add_dep("xgboost.sampler_type", "xgboost.booster", CondEqual$new("dart"))$
+    add_dep("xgboost.sample_type", "xgboost.booster", CondEqual$new("dart"))$
     add_dep("xgboost.normalize_type", "xgboost.booster", CondEqual$new("dart"))$
     add_dep("xgboost.rate_drop", "xgboost.booster", CondEqual$new("dart"))$
-    add_dep("xgboost.skip_drop", "xgboost.booster", CondEqual$new("dart"))$
-    add_dep("xgboost.one_drop", "xgboost.booster", CondEqual$new("dart"))$
-    add_dep("xgboost.max_leaves", "xgboost.grow_policy", CondEqual$new("lossguide"))
+    add_dep("xgboost.skip_drop", "xgboost.booster", CondEqual$new("dart"))
+#    add_dep("xgboost.one_drop", "xgboost.booster", CondEqual$new("dart"))$
+#    add_dep("xgboost.max_leaves", "xgboost.grow_policy", CondEqual$new("lossguide"))
 
-  parset = switch(par.set,
-      mixed = autoxgbparset.mixed,
-      unmixed = autoxgbparset)
-
-  parset$trafo = function(x, param_set) {
+  trafo = function(x, param_set) {
     if (!is.null(x$xgboost.max_leaves)) {
       x$xgboost.max_leaves = 2^x$xgboost.max_leaves
     }
@@ -105,9 +100,14 @@ autoxgboost_space = function(task, par.set = "unmixed", predict.type = "response
     x
   }
 
+  autoxgbparset$trafo = trafo
+  autoxgbparset.mixed$trafo = trafo
+
   if (task$task_type == "classif") {
     if ("twoclass" %in% task$properties) {
-      parset$add_param(ParamDbl$new("xgboost.scale_pos_weight", -10, 10))
+      wparam = ParamDbl$new("xgboosts.cale_pos_weight", -10, 10)
+      autoxgbparset$add_param(wparam)
+      autoxgbparset.mixed$add_param(wparam)
       objective = "binary:logistic"
       eval_metric = "error"
     } else {
@@ -140,7 +140,7 @@ autoxgboost_space = function(task, par.set = "unmixed", predict.type = "response
   # two ways to do this: impact.encoding.boundary *inside* the graph -> correct methods
   #                      impact.encoding.boundary *outside* the graph -> cheating but exact emulation
 
-# all cols with levels <= 'impact.encoding.boundary', on training or whole task depending on emulate_exactly
+  # all cols with levels <= 'impact.encoding.boundary', on training or whole task depending on emulate_exactly
   if (emulate_exactly) {
     encodetarget = selector_name(selector_less_or_equal_levels(impact.encoding.boundary)(task))
   } else {
@@ -152,7 +152,10 @@ autoxgboost_space = function(task, par.set = "unmixed", predict.type = "response
     po("encodeimpact") %>>%
     po("removeconstants") %>>%
     xgblearner
-  list(learner = GraphLearner$new(graph, predict_type = predict.type), searchspace = parset)
+
+  list(learner = GraphLearner$new(graph, predict_type = predict.type),
+    searchspace = autoxgbparset, searchspace.mixed = autoxgbparset.mixed,
+    resampling = rsmp("holdout", ratio = early.stopping.fraction)$instantiate(task))
 }
 
 selector_less_or_equal_levels = function(levels) {
--- todo-files/experiments.R
+++ todo-files/experiments.R
@@ -1,8 +1,11 @@
 
 
+devtools::load_all("mlr3pipelines")
+
 data.table::setDTthreads(1)
 devtools::document("autoxgboost3")
 
+library("data.table")
 devtools::load_all("autoxgboost3")
 
 
@@ -10,5 +13,32 @@ library("mlr3")
 
 task = tsk("iris")
 
-autoxgboost_model(task, tune.threshold = FALSE)
+space <- autoxgboost_space(task, tune.threshold = FALSE)
 
+space$learner$param_set
+names(space$searchspace)
+
+generate_design_random(space$searchspace, 10)$transpose()
+suggestion <- generate_design_random(space$searchspace.mixed, 1)$transpose()[[1]]
+
+
+space$learner$param_set$values <- mlr3misc::insert_named(space$learner$param_set$values, suggestion)
+
+resample(task, space$learner, space$resampling)
+
+for (setup in mlr3misc::transpose_list(CJ(
+  task = list("iris", "boston_housing", "pima"),
+  predict.type = c("response", "prob"),
+  impact.encoding.boundary = c(10, 1000000),
+  tune.threshold = FALSE,
+  emulate_exactly = c(FALSE, TRUE)))) {
+  setup$task = tsk(setup$task)
+  space <- do.call(autoxgboost_space, setup)
+  designs <- c(generate_design_random(space$searchspace, 10)$transpose(),
+    generate_design_random(space$searchspace.mixed, 10)$transpose())
+  for (sug in designs) {
+    lrn = space$learner$clone(deep = TRUE)
+    lrn$param_set$values <- mlr3misc::insert_named(lrn$param_set$values, sug)
+    resample(setup$task, space$learner, space$resampling)
+  }
+}
